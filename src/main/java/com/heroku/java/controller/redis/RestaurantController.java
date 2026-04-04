9b683a1d91d447ffcc46f277f481ba05eb23e03e
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-04 09:14:39 +0800

    Delete restaurant by id

--- src/main/java/com/heroku/java/controller/redis/RestaurantController.java
+++ src/main/java/com/heroku/java/controller/redis/RestaurantController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,5 +53,28 @@ public class RestaurantController {
       return e.getMessage();
     }
     return "";
+  }
+
+  @GetMapping("/{id}")
+  public Restaurant getRestaurantById(@PathVariable("id") long id) {
+    Restaurant restaurant = new Restaurant();
+    try {
+      return restaurantRespository.findById(String.valueOf(id)).orElse(restaurant);
+    } catch (Exception e) {
+      restaurant.setName(e.getMessage());
+      return restaurant;
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public String deleteRestaurantById(@PathVariable("id") long id) {
+    try {
+      Restaurant restaurant = new Restaurant();
+      restaurant = restaurantRespository.findById(String.valueOf(id)).orElse(restaurant);
+      restaurantRespository.delete(restaurant);
+    } catch (Exception e) {
+      return e.getMessage();
+    }
+    return "Restaurant " + id + " is deleted.";
   }
 }
